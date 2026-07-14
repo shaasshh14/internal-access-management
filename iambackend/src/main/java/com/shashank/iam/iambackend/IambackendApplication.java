32ac279843ddf8d2e22f18bf4a8f77b44843bbb6
shaasshh14 <shashankcoc4@gmail.com>
2026-07-15 00:28:50 +0530

    Initial commit - Spring Boot IAM Backend

---
+++ iambackend/src/main/java/com/shashank/iam/iambackend/IambackendApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.iam.iambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IambackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IambackendApplication.class, args);
+	}
+
+}
