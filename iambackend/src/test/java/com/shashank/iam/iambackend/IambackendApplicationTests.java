c93bd69a60144d00491efb4131de5f1e19239052
shaasshh14 <shashankcoc4@gmail.com>
2026-07-15 00:28:50 +0530

    Initial commit - Spring Boot IAM Backend

---
+++ iambackend/src/test/java/com/shashank/iam/iambackend/IambackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.iam.iambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IambackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
