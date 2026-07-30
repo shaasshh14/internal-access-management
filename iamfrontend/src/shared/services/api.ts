a4e33230b298dc5134026bf9f767ebce10062436
shaasshh14 <shashankcoc4@gmail.com>
2026-07-30 23:56:23 +0530

    jwt authetication setup

--- iamfrontend/src/shared/services/api.ts
+++ iamfrontend/src/shared/services/api.ts
@@ -4,6 +4,8 @@ const api = axios.create ({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 10000,
 
+    withCredentials: true,
+
     headers: {
         "Content-Type": "application/json",
     },
