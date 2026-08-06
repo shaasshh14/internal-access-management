e4c9d00e5c92f1f192493462ae0cce16ef75e3aa
shaasshh14 <shashankcoc4@gmail.com>
2026-08-06 23:52:00 +0530

    add centralized Axios instance with request and response interceptors

--- iamfrontend/src/shared/services/api.ts
+++ iamfrontend/src/shared/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { config } from "zod";
 
 const api = axios.create ({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -10,5 +11,24 @@ const api = axios.create ({
         "Content-Type": "application/json",
     },
 });
+
+api.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use (
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const originalRequest = error.config;
+        return Promise.reject(error);
+    }
+);
 
 export default api;
