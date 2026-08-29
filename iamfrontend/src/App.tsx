7290bb821c7154d54eb56432f6fd5f7780f0f288
shaasshh14 <shashankcoc4@gmail.com>
2026-08-29 23:57:03 +0530

    add global command palette, edit navbar

--- iamfrontend/src/App.tsx
+++ iamfrontend/src/App.tsx
@@ -1,5 +1,10 @@
 import AppRouter from "./app/routes/AppRouter";
+import { CommandPaletteProvider } from "./shared/components/CommandPalette/CommandPaletteContext";
 
 export default function App() {
-  return <AppRouter/>;
+  return (
+    <CommandPaletteProvider>
+      <AppRouter />
+    </CommandPaletteProvider>
+  );
 }
